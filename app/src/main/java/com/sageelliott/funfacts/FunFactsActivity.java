71fe08518218f71b7a60ea40d63597c67f71782b
Sage Elliott <wildtroll@gmail.com>
2016-06-26 19:50:53 -0700

    Declare view variables

--- app/src/main/java/com/sageelliott/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/sageelliott/funfacts/FunFactsActivity.java
@@ -2,12 +2,21 @@ package com.sageelliott.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    // declare our view variable
+    private TextView mFactTextView;
+    private Button mShowFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+
+
+
     }
 }
